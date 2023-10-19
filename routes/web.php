632057ba766b98b9130e9f209cfab5672d140cd1
afcyy <vazha.aptsiauri.1@btu.edu.ge>
2023-10-19 15:17:12 +0400

    Added Blades Task

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    $quizzes = [
+        [
+            "name" => "Quiz #1",
+            "img" => "https://plus.unsplash.com/premium_photo-1675024298717-02d612d9cc82?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8SVR8ZW58MHx8MHx8fDA%3D",
+            "status" => "Passed"
+        ],
+        [
+            "name" => "Quiz #2",
+            "img" => "https://plus.unsplash.com/premium_photo-1675024367645-68d0d6c30254?auto=format&fit=crop&q=80&w=2022&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "status" => "Upcoming"
+        ],
+        [
+            "name" => "Quiz #3",
+            "img" => "https://plus.unsplash.com/premium_photo-1675024226990-36dcb7252c62?auto=format&fit=crop&q=80&w=1970&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "status" => "Failed"
+        ],
+    ];
+
+    return view('welcome', compact('quizzes'));
+});
+
+Route::put('/subscribe', function (Request $request) {
+   dd($request->email . " Subscribed!");
+});
