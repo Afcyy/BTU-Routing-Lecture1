6704f4f4508bb362b1acca24a06fa6131ff6415a
Vazha Aptsiauri <vazha.aptsiauri.1@btu.edu.ge>
2023-10-28 09:48:31 +0400

    Added Quiz Model with Seeders.

--- database/seeders/QuizSeeder.php
+++ database/seeders/QuizSeeder.php
@@ -6,17 +6,17 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Quiz;
 
-class DatabaseSeeder extends Seeder
+class QuizSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            QuizSeeder::class,
-        ]);
+        Quiz::factory()
+            ->count(5)
+            ->create();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Quiz;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,23 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $quizzes = [
-        [
-            "name" => "Quiz #1",
-            "img" => "https://plus.unsplash.com/premium_photo-1675024298717-02d612d9cc82?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8SVR8ZW58MHx8MHx8fDA%3D",
-            "status" => "Passed"
-        ],
-        [
-            "name" => "Quiz #2",
-            "img" => "https://plus.unsplash.com/premium_photo-1675024367645-68d0d6c30254?auto=format&fit=crop&q=80&w=2022&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            "status" => "Upcoming"
-        ],
-        [
-            "name" => "Quiz #3",
-            "img" => "https://plus.unsplash.com/premium_photo-1675024226990-36dcb7252c62?auto=format&fit=crop&q=80&w=1970&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            "status" => "Failed"
-        ],
-    ];
+    $quizzes = Quiz::all();
 
     return view('welcome', compact('quizzes'));
 });
