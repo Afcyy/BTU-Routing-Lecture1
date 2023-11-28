a7d1c6c7c4ba3b19eafa14b989ccdba7a627b928
afcyy <vazha.aptsiauri.1@btu.edu.ge>
2023-11-28 12:16:23 +0400

    Modified Styles

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,51 +1,58 @@
 @extends('layout')
 
 @push('styles')
-    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 @endpush
 
 @section('content')
 
-    <div class="container">
-        <form action="{{ route('quiz.createOrUpdate', $quiz) }}" method="POST">
+    <div style="height: 90vh; display: flex; flex-direction: column; justify-content: center; align-items: center">
+        <form action="{{ route('quiz.createOrUpdate', $quiz) }}" method="POST" style="width: 75%;">
             @csrf
             @method('PUT')
 
-            <div class="form-group">
-                <label for="name" class="col-sm-2 col-form-label">Name</label>
-                <div class="col-sm-10">
-                    <input name="name" type="text" class="form-control" id="name" placeholder="name" value="{{ isset($quiz) ? $quiz->name : '' }}">
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="description" class="col-sm-2 col-form-label">Description</label>
-                <div class="col-sm-10">
-                    <input name="description" type="text" class="form-control" id="description" placeholder="Description" value="{{ isset($quiz) ? $quiz->description : '' }}">
-                </div>
+            <div class="my-3">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                <input name="name" value="{{ isset($quiz) ? $quiz->name : '' }}" type="text" id="name" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required>
             </div>
 
-            <div class="form-group">
-                <label for="exampleFormControlSelect1">Example select</label>
-                <select name="score" class="form-control" id="exampleFormControlSelect1">
+            <div class="my-3">
+                <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                <input name="description" value="{{ isset($quiz) ? $quiz->description : '' }}" type="text" id="description" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
+            </div>
+
+            <div class="my-3">
+                <label for="score" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Max score</label>
+                <select name="score" id="score" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     @for($i = 1; $i <= 10; $i++)
                         <option @if(isset($quiz) && $quiz->score == $i) selected @endif>{{ $i }}</option>
                     @endfor
                 </select>
             </div>
 
-            <div class="form-group">
-                <label for="deadline" class="col-sm-2 col-form-label">Deadline</label>
-                <div class="col-sm-10">
-                    <input name="deadline" type="text" class="form-control" id="deadline" placeholder="Deadline" value="{{ isset($quiz) ? $quiz->deadline : '' }}">
-                </div>
+            <div class="my-3">
+                <label for="deadline" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Deadline</label>
+                <input name="deadline" value="{{ isset($quiz) ? $quiz->deadline : '' }}" type="text" id="deadline" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required>
             </div>
 
-            <div class="form-group mt-3">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
+            <div class="my-3">
+                <label for="image" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
+                <input name="image" value="{{ isset($quiz) ? $quiz->image : '' }}" type="text" id="deadline" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light">
             </div>
+
+            <div class="my-3">
+                <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
+                <select name="status" id="status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option @if(isset($quiz) && $quiz->status == "active") selected @endif value="active">Active</option>
+                        <option @if(isset($quiz) && $quiz->status == "not_active") selected @endif value="not_active">Not Active</option>
+                </select>
+            </div>
+
+            <button type="submit"
+                    class="my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none w-full">
+                Submit
+            </button>
         </form>
     </div>
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,18 +12,34 @@
     @stack("styles")
 </head>
 <body class="antialiased">
-    @yield('content')
+    <div class="container">
 
-    <footer>
-        <form class="mb-3" action="/subscribe" method="POST">
-            @csrf
-            @method("PUT")
-            <label for="exampleFormControlInput1" class="form-label">Subscribe to our news letter</label>
-            <div style="display:flex; width: 100%; justify-content: space-around">
-                <input name="email" type="email" class="form-control col-9" id="exampleFormControlInput1" placeholder="name@example.com" style="width: 75%">
-                <button type="submit" class="btn btn-primary" style="width: 23%">Subscribe</button>
+        <nav class="bg-white border-gray-200 dark:bg-gray-900">
+            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="https://btu.edu.ge/wp-content/uploads/2021/06/BTU_Logo_new-1.svg" class="h-16" alt="BTU Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">BTU</span>
+                </a>
+                <div class="flex items-center space-x-6 rtl:space-x-reverse">
+                    <p class="text-sm  text-gray-500 dark:text-white hover:underline">Vazha Aptsiauri</p>
+                    <p href="#" class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Quizzes</p>
+                </div>
             </div>
-        </form>
-    </footer>
+        </nav>
+
+        @yield('content')
+
+        <footer class="min-w-fit">
+            <form class="mb-3" action="/subscribe" method="POST">
+                @csrf
+                @method("PUT")
+                <label for="exampleFormControlInput1" class="form-label">Subscribe to our news letter</label>
+                <div style="display:flex; width: 100%; justify-content: space-around">
+                    <input name="email" type="email" class="form-control col-9" id="exampleFormControlInput1" placeholder="name@example.com" style="width: 75%">
+                    <button type="submit" class="btn btn-primary" style="width: 23%">Subscribe</button>
+                </div>
+            </form>
+        </footer>
+    </div>
 </body>
 </html>
